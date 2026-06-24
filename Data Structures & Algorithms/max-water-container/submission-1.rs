80fd855ef27c66b14e7e93cf816ca843cae47ea8
Joshua Isika <joshuaiska@gmail.com>
2026-06-24 16:30:59 +0300

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn max_area(heights: Vec<i32>) -> i32 {
+    let mut left= 0;
+    let mut right = heights.len() -1;
+    let mut max =0;
+    while left < right{
+         let   area = heights[left].min(heights[right]) * (right -left) as i32;
+         max = max.max(area);
+         if heights[left] <= heights[right]{
+            left +=1;
+            }else{
+               right -=1;
+          }
+     }
+    max
+    }
+}
